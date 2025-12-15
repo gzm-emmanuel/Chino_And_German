9cb281cf2a33109e2237e35a38360d9192f03218
dipf090724mgtgtra7-droid <dipf090724mgtgtra7@cbtis217.edu.mx>
2025-12-15 15:01:47 -0600

    termine el formulario de empleados y pude ponerele los error.provinder para terminarlo

---
+++ chinoandgerman/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CC1_PC50
+ * Fecha: 08/12/2025
+ * Hora: 03:08 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Windows.Forms;
+
+namespace detallesdecliente
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
